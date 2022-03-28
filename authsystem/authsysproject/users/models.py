c44674b1f7437557e9e51677ad129fe5551fcfe2
AlJinx98 <56905207+AlJinx98@users.noreply.github.com>
2022-03-28 17:58:19 +0100

    Update models.py

--- authsystem/authsysproject/users/models.py
+++ authsystem/authsysproject/users/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
 ## Film Class// Title. Age. etc 
-##Class Film: Title =  
-
+class theFilm(Models.models):
+    Title = models.CharField(max_length=150)
+    Desciption = models.TextField()
+    ageRating = models.IntegerField(max_length=3)
 ## Booking Class//
+class bookIt(Models.model):
+    ticketType = models.TextChoices()
+    Cost = models.IntegerField()
+    clubDiscount = models.BinaryField()
 
 ## Clubs Class// 
 
 ## Showing Class// 
-
+class showings(Models.models):
+    showTimings = models.TimeField()
+    showDates = models.DateField()
 ## Screen Class// 
 
 ##Ticket // 
 
 ## Token Class// 
-
-## 
